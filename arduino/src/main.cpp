cab460c5b8d39746e0a5f3d2f75bf17c009f33ee
Jin Sato <jsato.jtoz@gmail>
2021-02-14 17:48:53 +0000

    Add Arduino LED blink test via rosserial

---
+++ arduino/src/main.cpp
@@ -0,0 +1,33 @@
+#include <ros.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle nh;
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+	digitalWrite(13, HIGH);
+	delay(100);
+	digitalWrite(13, LOW);
+	delay(100);
+	digitalWrite(13, HIGH);
+	delay(100);
+	digitalWrite(13, LOW);
+	delay(100);
+	digitalWrite(13, HIGH);
+	delay(100);
+	digitalWrite(13, LOW);
+	delay(100);
+}
+
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+
+void setup(){
+	pinMode(13, OUTPUT);
+	digitalWrite(13, HIGH);
+	nh.initNode();
+	nh.subscribe(sub);
+}
+
+void loop(){
+	nh.spinOnce();
+	delay(1);
+}
